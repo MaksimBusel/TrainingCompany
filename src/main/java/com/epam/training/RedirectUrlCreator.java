34c3697fc17e5da8185061cde22e355c04996bf8
FilippStankevichTR <52537544+FilippStankevichTR@users.noreply.github.com>
2020-02-05 18:04:07 +0300

    Update RedirectUrlCreator.java

--- src/main/java/com/epam/training/RedirectUrlCreator.java
+++ src/main/java/com/epam/training/RedirectUrlCreator.java
@@ -2,6 +2,8 @@ package main.java.com.epam.training;
 
 public class RedirectUrlCreator {
 
+    
+    
     public static String create(String command){
         return "{request.contextPath}controller?command=" + command;
     }
